2d3f0156c0ea7e4e335f6fe1739c15d8f0c255cb
sujtech <84857871+sujtech@users.noreply.github.com>
2021-05-26 18:37:21 -0500

    Add files via upload

---
+++ CTM/CTM/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CTM.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IFizzBuzzService _service;
+        public  ValuesController(IFizzBuzzService service)
+        {
+            _service = service;
+        }
+
+        // GET api/values/5
+        [HttpGet("{n}")]
+        public List<string> Get(int n)
+        {
+            if (n <= 0)
+            {
+                return new List<string> { "Value cannot be less than 0 or 0" };
+            }
+            else if (n > 100)
+            {
+                return new List<string> { "Value cannot be more than 100" };
+            }
+            else
+            {
+                return _service.FizzBuzz(n);
+            }
+        }
+
+       
+    }
+}
